f5cff933d17915991d22976ae91302a2621ff0cc
Viraj Ranaware <vranawar@uncc.edu>
2022-09-06 00:28:41 -0400

    added loops problem set 1

---
+++ src/main/java/org/example/loops/Loops_02.java
@@ -0,0 +1,15 @@
+package org.example.loops;
+
+public class Loops_02
+{
+    public static void main( String[] args )
+    {
+        for(; ;) {
+            System.out.println("Viru");
+        }
+    }
+}
+
+/*
+Infinite Loop
+ */
